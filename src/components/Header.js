338570fc52c416395dab86d9cc83722c05dab503
Swee En Yap <sweeenyap@Swees-MacBook-Pro.local>
2023-08-27 15:51:07 +0800

    readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,6 @@ const Header = () => {
                     <Link to='/' className="navbar-brand">
                         <img className="img-fluid" src="assets/images/logo.png" alt="" />
                     </Link>
-
-                    <div className="collapse navbar-collapse navbar-right" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto text-center" id="nav">
-                            <li className="nav-item active">
-                                <Link to='/' className='nav-link'>Home</Link>
-                            </li>
-                            <li className="nav-item active">
-                                <Link to='/favorites' className='nav-link'>My Favorites</Link>
-                            </li>
-                        </ul>
-                    </div>
                 </nav>
             </div>
         </header >
